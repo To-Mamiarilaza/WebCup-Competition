b7a9a9064b6f83a158299e474f890ad62be96812
To-Mamiarilaza <mamiarilaza.to@gmail.com>
2024-05-05 02:21:16 +0300

    Page listes des produits terminé

--- Front-End/src/app/pages/list-produit/list-produit.component.ts
+++ Front-End/src/app/pages/list-produit/list-produit.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProduitComponent } from '../home/produit/produit.component';
 
 @Component({
   selector: 'app-list-produit',
   standalone: true,
-  imports: [],
+  imports: [ProduitComponent],
   templateUrl: './list-produit.component.html',
   styleUrl: './list-produit.component.scss'
 })

43da96e332e82393698a38f386cab6dd466a4cc5
Johananjaratiana <118510387+Johananjaratiana@users.noreply.github.com>
2024-05-05 01:42:19 +0300

    update curl with Authorization

--- Back-End/other/curl.sh
+++ Back-End/other/curl.sh
@@ -21,22 +21,22 @@ curl http://127.0.0.1:8000/api/login \
 
 curl http://127.0.0.1:8000/api/test \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI"
+  -H "ApiToken: 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI"
 
 # --------------------------------------------- GET
 curl -X GET http://127.0.0.1:8000/api/mode_paiements \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI"
+  -H "ApiToken: 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI"
 
 curl -X GET http://127.0.0.1:8000/api/equivalence_devises \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI"
+  -H "ApiToken: 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI"
 
 
 # --------------------------------------------- AJOUT JETON
 curl http://127.0.0.1:8000/api/transaction_jetons \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI" \
+  -H "ApiToken: 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI" \
   -d '{
     "valeur_devise": 10,
     "montant": 1000
@@ -46,7 +46,7 @@ curl http://127.0.0.1:8000/api/transaction_jetons \
 # --------------------------------------------- ACHAT PRODUIT
 curl http://127.0.0.1:8000/api/achat_produits \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI" \
+  -H "ApiToken: 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI" \
   -d '{
     "id_produit": 3,
   }'
@@ -54,18 +54,18 @@ curl http://127.0.0.1:8000/api/achat_produits \
 # --------------------------------------------- ACHAT PRODUIT
 curl http://127.0.0.1:8000/api/transactions \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI" \
+  -H "ApiToken: 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI" \
   -X GET
 
 # --------------------------------------------- VENTE
 curl http://127.0.0.1:8000/api/historique_ventes \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI" \
+  -H "ApiToken: 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI" \
   -X GET
 
 curl http://127.0.0.1:8000/api/annulation_vente \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI" \
+  -H "ApiToken: 2FmaZ6hC8zmDnmnzGvqkHccIE9peTHD0rHNiEpWpdIohpzKcTiAmC80CN4MI" \
   -X GET \
   -d '{
     "id_produit": 6

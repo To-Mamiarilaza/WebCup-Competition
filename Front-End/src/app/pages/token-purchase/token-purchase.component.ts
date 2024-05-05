db288883245839258799777eaff5254b0bcdfe7b
Johananjaratiana <118510387+Johananjaratiana@users.noreply.github.com>
2024-05-05 03:58:57 +0300

    Left add

--- Front-End/src/app/pages/token-purchase/token-purchase.component.ts
+++ Front-End/src/app/pages/token-purchase/token-purchase.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { EquivalenceDevise } from '../../models/EquivalenceDevise';
+import { CommonModule } from '@angular/common'; // Import du CommonModule
+import { env } from '../../environments/env';
 
 @Component({
   selector: 'app-token-purchase',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './token-purchase.component.html',
   styleUrl: './token-purchase.component.scss'
 })
-export class TokenPurchaseComponent {
+export class TokenPurchaseComponent  implements OnInit{
+  equivalenceDevises: EquivalenceDevise[] = []
+  deviseSelected: EquivalenceDevise | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<EquivalenceDevise[]>(env.baseUrl + "/equivalence_devises")
+    .subscribe(data => {
+      this.equivalenceDevises = data
+    })
+  }
+  onDeviseSelect(devise: EquivalenceDevise): void {
+    this.deviseSelected = devise;
+  }
+
+  
 }

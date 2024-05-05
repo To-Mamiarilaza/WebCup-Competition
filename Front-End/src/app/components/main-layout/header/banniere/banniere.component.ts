a30f024b5bf91c6b399b49b0ec70832a76f06e09
To-Mamiarilaza <mamiarilaza.to@gmail.com>
2024-05-05 09:13:19 +0300

    Fix/design

--- Front-End/src/app/components/main-layout/header/banniere/banniere.component.ts
+++ Front-End/src/app/components/main-layout/header/banniere/banniere.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-banniere',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './banniere.component.html',
   styleUrl: './banniere.component.scss'
 })
